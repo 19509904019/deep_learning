36061b8408e62590488c3d3dc42bae8905cd75e6
yujun <yujunaddress@163.com>
2022-04-20 11:02:21 +0800

    change

--- my_model1/read_data1.py
+++ my_model1/read_data1.py
@@ -1,5 +1,5 @@
 import cst.results
-from my_model import metamaterial_01
+from my_model1 import metamaterial_01
 
 project = cst.results.ProjectFile(r'C:\Users\Dell\Desktop\simulation\123.cst', allow_interactive=True)  # 允许窗口打开读取
 s11 = project.get_3d().get_result_item(r'1D Results\S-Parameters\SZmax(1),Zmax(1)')
