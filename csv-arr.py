04a93a0406da064474d6e19995321dd84328b98e
yujun <yujunaddress@163.com>
2022-05-05 20:46:58 +0800

    change

--- csv-arr.py
+++ csv-arr.py
@@ -1,5 +1,6 @@
 import csv
 import torch
+import pandas as pd
 
 targets = []
 with open(r"C:\Users\Dell\Desktop\geometric_parameters.csv") as csvfile:
@@ -7,5 +8,16 @@ with open(r"C:\Users\Dell\Desktop\geometric_parameters.csv") as csvfile:
     for row in reader:  # each row is a list
         targets.append(row)
 
-targets = torch.tensor(targets, dtype=torch.float32)
-print(targets[0])
+
+# targets = torch.tensor(targets, dtype=torch.float32)
+# print(targets[0])
+
+
+def load_csv(path):
+    data_read = pd.read_csv(path)
+    list = data_read.values.tolist()
+    data = torch.tensor(list)
+    return data
+
+# targets = load_csv(r"C:\Users\Dell\Desktop\geometric_parameters.csv")
+# print(targets[0])
