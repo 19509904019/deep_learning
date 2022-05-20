21c638627fd92048a17a41e19c2ad4e9a85c04ee
yujun <yujunaddress@163.com>
2022-05-20 23:53:07 +0800

    Changes

---
+++ network/my_network2/GPU_train_convnet.py
@@ -0,0 +1,76 @@
+import csv
+import torch
+from network_convnet import *
+import torch.nn as nn
+from torch.optim import *
+import time
+
+s11 = []
+parameters = []
+# 电磁响应
+with open(r"C:\Users\user2\Desktop\dataset\dataset1\train_set\s11.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
+    for row in reader:  # each row is a list
+        s11.append(row)
+    s11 = torch.tensor(s11, dtype=torch.float32)
+
+# 几何参数
+with open(r"C:\Users\user2\Desktop\dataset\dataset1\train_set\parameters.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
+    for row in reader:  # each row is a list
+        parameters.append(row)
+    parameters = torch.tensor(parameters, dtype=torch.float32)
+
+# print(s11)
+# print(type(s11))
+# print(parameters)
+# print(type(parameters))
+
+# GPU加速
+device = torch.device('cuda:0')
+
+# 生成网络
+mymodel = MyModel().to(device)
+
+# 损失函数
+loss_F = nn.L1Loss().to(device)
+
+# 优化器
+lr = 1e-3
+optimizer = Adam(mymodel.parameters(), lr=lr)
+
+# 计数
+train_loss = 0
+flag = True
+
+while flag:
+    # 训练开始
+    mymodel.train()
+    for number in range(len(s11)):
+        train_loss += 1
+        data = torch.reshape(s11[number], (1, 1, -1)).to(device)
+        outputs = mymodel(data).to(device)
+        # print(outputs)
+        # 结构参数
+        parameter = torch.reshape(parameters[number], (1, -1)).to(device)
+        # print(parameter)
+        # 计算损失函数
+        loss = loss_F(outputs, parameter).to(device)
+        # print(loss)
+
+        # 优化模型
+        optimizer.zero_grad()  # 梯度清零
+        loss.backward()
+        optimizer.step()
+
+        if train_loss % 2400 == 0:
+            print(f'训练轮数:{train_loss // 2400},loss:{loss.item()}')
+
+        if loss.item() < 0.02:
+            flag = False
+            print(f"训练已结束,当前loss:{loss.item()}")
+            break
+
+# 保存模型
+torch.save(mymodel, 'mymodel1.pth')
+print("保存成功!")
---
+++ network/my_network2/GPU_use_network.py
@@ -0,0 +1,53 @@
+from network_convnet import *
+import csv
+import torch
+
+
+# 计算相对光谱误差
+def RSE(r, g):
+    a = torch.sqrt(torch.sum((r - g) ** 2)) / torch.sqrt(torch.sum(r ** 2))
+    return a
+
+
+# 测试数据
+test_s11 = []
+with open(r"C:\Users\user2\Desktop\dataset\dataset1\test_set\s11.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
+    for row in reader:  # each row is a list
+        test_s11.append(row)
+    test_s11 = torch.tensor(test_s11, dtype=torch.float32)
+
+# 几何参数
+test_parameters = []
+with open(r"C:\Users\user2\Desktop\dataset\dataset1\test_set\parameters.csv") as csvfile:
+    reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)  # change contents to floats
+    for row in reader:  # each row is a list
+        test_parameters.append(row)
+    test_parameters = torch.tensor(test_parameters, dtype=torch.float32)
+
+# 加载已经训练好的模型
+mymodel = torch.load('mymodel1.pth')
+
+# 进行测试
+device = torch.device('cuda:0')  # GPU加速
+outputs = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=torch.float32)
+outputs = torch.reshape(outputs, (1, -1)).to(device)  # 存放预测结果的总和
+parameters = torch.tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0], dtype=torch.float32)
+parameters = torch.reshape(parameters, (1, -1)).to(device)  # 存放真实结果的总和
+# 开始测试
+mymodel.eval()
+with torch.no_grad():  # 无需梯度
+    for number in range(len(test_s11)):
+        data = torch.reshape(test_s11[number], (1, 1, -1)).to(device)  # 600个测试数据
+        output = mymodel(data).to(device)  # 电磁响应所对应预测的结构参数
+        outputs += output
+        # 测试数据的真实结构参数
+        parameter = torch.reshape(test_parameters[number], (1, -1)).to(device)
+        parameters += parameter
+        # 真实结果与预测结果相对光谱误差
+        result_1 = RSE(parameter, output)
+        print(f"预测结果为：{output},真实结果为：{parameter},相对光谱误差为：{result_1}")
+
+    # 整体的相对光谱误差
+    result_2 = RSE(parameters, outputs)
+    print(f'整体的相对光谱误差为:{result_2}')
