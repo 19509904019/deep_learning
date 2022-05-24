8588083a7aa494f1662320c8fbfcc3de5e6f9ae0
yujun <yujunaddress@163.com>
2022-05-24 11:44:45 +0800

    change

--- network/my_network2/network_convnet.py
+++ network/my_network2/network_convnet.py
@@ -12,12 +12,12 @@ class MyModel(nn.Module):
             nn.MaxPool1d(2),
             # 第二层卷积
             nn.Conv1d(16, 32, kernel_size=2),
-            nn.ReLU(),
             nn.MaxPool1d(3),
             # 压平
             nn.Flatten(),
             # 全连接层
             nn.Linear(2656, 1000),
+            nn.ReLU(),
             nn.Linear(1000, 50),
             # nn.Linear(50, 1000)
         )
@@ -26,32 +26,32 @@ class MyModel(nn.Module):
         self.model2 = nn.Sequential(
             # 第一层卷积
             nn.Conv1d(1, 32, kernel_size=2),
-            nn.ReLU(),
             nn.MaxPool1d(2),
             nn.BatchNorm1d(32),
             # 第二层卷积
             nn.Conv1d(32, 64, kernel_size=2),
-            nn.ReLU(),
             nn.MaxPool1d(2),
             nn.BatchNorm1d(64),
             # 第三层卷积
             nn.Conv1d(64, 128, kernel_size=2),
-            nn.ReLU(),
             nn.MaxPool1d(2),
             nn.BatchNorm1d(128),
             # 第四层卷积
             nn.Conv1d(128, 256, kernel_size=2),
-            nn.ReLU(),
             nn.MaxPool1d(2),
             nn.BatchNorm1d(256),
             nn.Flatten(),
             # 全连接层
             nn.Linear(512, 1024),
+            nn.ReLU(),
             nn.Linear(1024, 1024),
             nn.Dropout(0.5),
             nn.Linear(1024, 500),
+            nn.ReLU(),
             nn.Linear(500, 128),
+            nn.ReLU(),
             nn.Linear(128, 50),
+            nn.ReLU(),
             nn.Linear(50, 10)
         )
 
