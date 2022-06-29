5222fb3140d8165583607d582e9f8afe7bc56e99
yujun <yujunaddress@163.com>
2022-06-29 16:25:05 +0800

    Changes

--- picture_processing/get_01_array.py
+++ picture_processing/get_01_array.py
@@ -4,7 +4,7 @@ import random
 
 def get_0_1_array(array, rate=random.randint(3, 7) * 0.1):
     """按照数组模板生成对应的0-1矩阵，rate采用随机的方式"""
-    zeros_num = int(array.size * rate)  # 根据0的比率来得到 0的个数
+    zeros_num = int(array.size * rate)  # 根据0的比率来得到0的个数
     new_array = np.ones(array.size)  # 生成与原来模板相同的矩阵，全为1
     new_array[:zeros_num] = 0  # 将一部分换为0
     np.random.shuffle(new_array)  # 将0和1的顺序打乱
@@ -17,11 +17,11 @@ def get_0_1_array(array, rate=random.randint(3, 7) * 0.1):
 
 if __name__ == '__main__':
     count = 0
-    for i in range(10000):
+    for i in range(30000):
         count += 1
         a = get_0_1_array(np.eye(8))
         # print(a)
-        f = open(r'C:\Users\12414\Desktop\matrix\%d.txt' % count, 'a')
+        f = open(r'C:\Users\12414\Desktop\matrix\%d-matrix.txt' % count, 'a')
         for i in range(a.shape[0]):
             for j in range(a.shape[1]):
                 f.write(str(a[i][j]) + '\n')
