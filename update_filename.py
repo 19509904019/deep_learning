a7eff882d3091a829b95a2f1eb47b82c8faafd55
yujun <yujunaddress@163.com>
2022-07-18 22:31:54 +0800

    Changes

--- update_filename.py
+++ update_filename.py
@@ -4,12 +4,13 @@ path = r'C:\Users\12414\Desktop\matrix\6'
 
 # 获取该目录下所有文件，存入列表中
 fileList = os.listdir(path)
-
-n = 25000
+# 按顺序排列
+fileList.sort(key=lambda x: int(x[:-11]))
+n = 20000
 for i in fileList:
     n += 1
     # 设置旧文件名（就是路径+文件名）
-    oldname = path + os.sep + fileList[n-25001]  # os.sep添加系统分隔符
+    oldname = path + os.sep + fileList[n - 20001]  # os.sep添加系统分隔符
 
     # 设置新文件名
     newname = path + os.sep + f'{n}-matrix' + '.txt'
